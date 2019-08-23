bf75af746000cae60ed64c76ad2502abd0fa2de8
Marlon Leoner <marlon.leoner@gmail.com>
2019-08-23 13:47:41 -0300

    Criação da classe contendo a função main()

---
+++ src/main/java/Extractor/Extractor.java
@@ -0,0 +1,12 @@
+package Extractor;
+
+import View.ExtractorGUI;
+
+public class Extractor {
+
+   public static void main(String[] args) {
+
+      ExtractorGUI gui = new ExtractorGUI();
+      gui.setVisible(true);
+   }
+}

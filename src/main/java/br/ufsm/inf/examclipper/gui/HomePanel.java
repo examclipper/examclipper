806d7b66a6056231f73e03156387e7c3bc859915
Marlon Leoner <marlon.leoner@gmail.com>
2019-08-27 16:59:49 -0300

    Refatoração do HomePanel
    - Adicionado opções para usuário escolher entre criar ou abrir um projeto(ainda não implementadas), e abrir um PDF.

--- src/main/java/br/ufsm/inf/examclipper/gui/HomePanel.java
+++ src/main/java/br/ufsm/inf/examclipper/gui/HomePanel.java
@@ -1,86 +1,137 @@
 package br.ufsm.inf.examclipper.gui;
 
 import br.ufsm.inf.examclipper.ExamClipperGUI;
-import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Cursor;
-
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.border.EmptyBorder;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 
 public class HomePanel extends JPanel {
 
-   private ExamClipperGUI gui;
+   private final ExamClipperGUI gui;
 
-   private JButton loadButton;
+   public HomePanel(ExamClipperGUI gui) {
+      this.gui = gui;
 
-   public HomePanel(ImageIcon icon, ImageIcon logo, ActionListener actionListener) {
-      initComponents(icon, logo, actionListener);
+      initComponents();
    }
 
-   private void initComponents(ImageIcon icon, ImageIcon logo, ActionListener actionListener) {
+   private void initComponents() {
+      ImageIcon icLogo        = new ImageIcon(getClass().getClassLoader().getResource("ic_logo_200.png"));
+      ImageIcon icNewProject  = new ImageIcon(getClass().getClassLoader().getResource("ic_folder_plus_solid_16.png"));
+      ImageIcon icOpenProject = new ImageIcon(getClass().getClassLoader().getResource("ic_folder_solid_16.png"));
+      ImageIcon icOpenPDF     = new ImageIcon(getClass().getClassLoader().getResource("ic_pdf_16.png"));
+      
+      setLayout(new GridLayout(2, 1));
 
-      BorderLayout borderLayout = new BorderLayout();
-      setLayout(borderLayout);
+      Dimension size = new Dimension(200, 40);
+      EmptyBorder border = new EmptyBorder(10, 20, 10, 20);
 
-      JLabel logoLabel = new JLabel(logo);
+      JLabel logoLabel = new JLabel(icLogo);
       logoLabel.setBorder(new EmptyBorder(10, 0, 25, 0));
 
-      loadButton = new JButton("Load file");
-      loadButton.setBackground(null);
-      loadButton.setBorder(new EmptyBorder(0, 20, 0, 20));
-      loadButton.setIcon(icon);
-      loadButton.addActionListener(actionListener);
-      loadButton.setPreferredSize(new Dimension(200, 50));
-      loadButton.addMouseListener(new MouseAdapter() {
-         Color oldcolor = loadButton.getForeground();
-
-         public void mouseEntered(MouseEvent me) {
-            loadButton.setForeground(Color.red);
-            setCursor(new Cursor(Cursor.HAND_CURSOR));
-         }
-
-         public void mouseExited(MouseEvent me) {
-            loadButton.setForeground(oldcolor);
-            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-         }
-      });
-
-      JLabel newProject = new JLabel("Novo Projeto");
-      newProject.setBorder(new EmptyBorder(0, 20, 0, 20));
-      newProject.setIcon(icon);
+      JLabel newProject = new JLabel("  Criar um novo projeto");
+      newProject.setAlignmentX(CENTER_ALIGNMENT);
+      newProject.setBorder(border);
+      newProject.setMaximumSize(size);
+      newProject.setMinimumSize(size);
+      newProject.setIcon(icNewProject);
       newProject.setOpaque(true);
-      newProject.setPreferredSize(new Dimension(200, 50));
       newProject.addMouseListener(new MouseAdapter() {
-         Color oldcolor   = newProject.getForeground();
+         Color oldFgcolor = newProject.getForeground();
          Color oldBgColor = newProject.getBackground();
 
          public void mouseEntered(MouseEvent me) {
-            newProject.setForeground(Color.RED);
-            newProject.setBackground(Color.BLACK);
+            newProject.setForeground(Color.BLACK);
+            newProject.setBackground(Color.LIGHT_GRAY);
             setCursor(new Cursor(Cursor.HAND_CURSOR));
          }
 
          public void mouseExited(MouseEvent me) {
-            newProject.setForeground(oldcolor);
+            newProject.setForeground(oldFgcolor);
             newProject.setBackground(oldBgColor);
             setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
          }
+         
+         public void mouseClicked(MouseEvent me) {
+            // TODO
+         }
       });
-      
-      JPanel buttonsPanel = new JPanel();
-      buttonsPanel.add(loadButton);
-      buttonsPanel.add(newProject);
 
-      add(logoLabel, BorderLayout.NORTH);
-      add(buttonsPanel, BorderLayout.CENTER);
+      JLabel openProject = new JLabel("  Abrir um projeto");
+      openProject.setAlignmentX(CENTER_ALIGNMENT);
+      openProject.setBorder(border);
+      openProject.setMaximumSize(size);
+      openProject.setMinimumSize(size);
+      openProject.setIcon(icOpenProject);
+      openProject.setOpaque(true);
+      openProject.addMouseListener(new MouseAdapter() {
+         Color oldFgcolor = newProject.getForeground();
+         Color oldBgColor = newProject.getBackground();
+
+         public void mouseEntered(MouseEvent me) {
+            openProject.setForeground(Color.BLACK);
+            openProject.setBackground(Color.LIGHT_GRAY);
+            setCursor(new Cursor(Cursor.HAND_CURSOR));
+         }
+
+         public void mouseExited(MouseEvent me) {
+            openProject.setForeground(oldFgcolor);
+            openProject.setBackground(oldBgColor);
+            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+         }
+         
+         public void mouseClicked(MouseEvent me) {
+            // TODO
+         }
+      });
+
+      JLabel openPDF = new JLabel("  Abrir um PDF");
+      openPDF.setAlignmentX(CENTER_ALIGNMENT);
+      openPDF.setBorder(border);
+      openPDF.setMaximumSize(size);
+      openPDF.setMinimumSize(size);
+      openPDF.setIcon(icOpenPDF);
+      openPDF.setOpaque(true);
+      openPDF.addMouseListener(new MouseAdapter() {
+         Color oldFgcolor = newProject.getForeground();
+         Color oldBgColor = newProject.getBackground();
+
+         public void mouseEntered(MouseEvent me) {
+            openPDF.setForeground(Color.BLACK);
+            openPDF.setBackground(Color.LIGHT_GRAY);
+            setCursor(new Cursor(Cursor.HAND_CURSOR));
+         }
+
+         public void mouseExited(MouseEvent me) {
+            openPDF.setForeground(oldFgcolor);
+            openPDF.setBackground(oldBgColor);
+            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+         }
+         
+         public void mouseClicked(MouseEvent me) {
+            gui.showOpenPDFDialog();
+         }
+      });
+
+      JPanel buttonsPanel = new JPanel();
+      buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+      buttonsPanel.add(newProject);
+      buttonsPanel.add(openProject);
+      buttonsPanel.add(openPDF);
+
+      add(logoLabel);
+      add(buttonsPanel);
    }
 }
